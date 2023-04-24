b3980f6cc531547ad4dad60102103b1edac67e02
Kim <ktla3223@gmail.com>
2023-04-24 13:19:47 -0700

    Created functions for add, subtract and multiply through lines 1-11

---
+++ app.js
@@ -0,0 +1,12 @@
+const add = function(a, b) {
+    return a + b;
+  };
+  
+const subtract = function(a, b) {
+      return a - b;
+  };
+
+const multiply = function(a) {
+    return a.reduce((total, current) => total * current);
+  };
+
