59dc5894d4b12d402eb0fc7b0930aff45d2ff9e1
Kim <ktla3223@gmail.com>
2023-04-24 13:40:45 -0700

    Created function for division

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const subtract = function(a, b) {
       return a - b;
   };
 
-const multiply = function(a) {
-    return a.length ? a.reduce((total, current) => total * current) : 0;
+const multiply = function(array) {
+    return array.length ? a.reduce((total, current) => total * current) : 0;
   };
 
+const division = function (a, b) {
+    if(a === 0) return "ERROR";
+
+}
