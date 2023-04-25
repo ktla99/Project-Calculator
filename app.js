d2c9c2ce518621430e9d8d7c7ade306c186e4c48
Kim <ktla3223@gmail.com>
2023-04-25 11:56:06 -0700

    Converted everything to operator and numbers

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-const restart = document.querySelector(".restart");
-const positiveNegative = document.querySelector(".positiveNegative");
-const percentage = document.querySelector(".percentage");
-const equal = document.querySelector(".equal");
-const decimal = document.querySelector(".dot");
+const clear = document.querySelector("#clear");
+const operator = document.querySelectorAll('#operator');
+const numbers = document.querySelectorAll('#number');
 
-const zero = document.querySelector(".numberZero");
-const one = document.querySelector(".numberOne");
-const two = document.querySelector(".numberTwo");
-const three = document.querySelector(".numberThree");
-const four = document.querySelector(".numberFour");
-const five = document.querySelector(".numberFive");
-const six = document.querySelector(".numberSix");
-const seven = document.querySelector(".numberSeven");
-const eight = document.querySelector(".numberEight");
-const nine = document.querySelector(".numberNine");
 
 function add(a, b) {
     const addition = document.querySelector(".addition");
@@ -44,5 +32,7 @@ function division(a, b) {
     }
 };
 
-function 
+function resetCalc () {
+
+}
 
