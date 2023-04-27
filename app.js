7fa4f0a0dcd8a2d36f934a1ed41224ef3d178c9a
Kim <ktla3223@gmail.com>
2023-04-27 11:33:19 -0700

    Create class calculator to take all the inputs and functions for the calculator and set the text elements to the place

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+  }
+}
+
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalButton = document.querySelector('[data-equal]');
