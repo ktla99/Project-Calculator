cb59f7058cd0aa7e4da3fbe02bf178f576af4f2c
Kim <ktla3223@gmail.com>
2023-04-28 10:02:28 -0700

    Added more to updateDisplay and getDisplayNumber

--- app.js
+++ app.js
@@ -59,9 +59,23 @@ class Calculator {
   }
 
   getDisplayNumber(number) {
-    const floatNumber = parseFloat(number)
-    if(isNaN(floatNumber)) return ''
-    return number.toLocaleString('en')
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    
+    if(isNaN(integerDigits)) {
+      integerDisplay = ''
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', {
+        maximumFractionDigits: 0
+      })
+    }
+    if (decimalDigits !== null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } else {
+      return integerDisplay
+    }
   }
 
   updateDisplay() {
@@ -70,6 +84,8 @@ class Calculator {
     if(this.operation != null) {
       this.previousOperandTextElement.innerText = 
       `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    } else {
+      this.previousOperandTextElement.innerText = ''
     }
   }
 }
